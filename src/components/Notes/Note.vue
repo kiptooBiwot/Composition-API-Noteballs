11dba88089cdfbe9fd0decbfa99ed1cdc7ea22de
Elvis <elviskb@gmail.com>
2022-11-23 18:34:44 +0300

    add new note & character count

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,49 @@
+<script setup>
+/**
+ * imports
+ */
+import { computed } from 'vue'
+
+/**
+ * Props
+ */
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true
+  }
+})
+
+/**
+ * Computed
+ */
+
+const characterLength = computed(() => {
+  let length = props.note.content.length
+  const chars = length > 1 ? 'characters' : 'character'
+  return length + ' ' + chars
+  // return answer
+})
+
+</script>
+
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+      <div class="has-text-right has-text-grey-light mt-2">
+        <small>{{ characterLength }}</small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,11 +1,69 @@
 <script setup>
+/**
+ * Imports
+ */
+
+import { ref } from 'vue';
+import Note from '@/components/Notes/Note.vue'
+
+
+/**
+ * Notes
+ */
+const newNote = ref('')
+const newNoteRef = ref(null)
+const notes = ref([
+  {
+    id: 1,
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, saepe? Magnam ea quam magni sapiente debitis, minus in iste autem nihil a.Recusandae mollitia in reprehenderit excepturi, modi ex odio.'
+  }
+])
+
+/**
+ * Methods
+ */
+
+const addNote = () => {
+  const currentDate = new Date().getTime()
+  const id = currentDate.toString()
+
+  let note = {
+    id,
+    content: newNote.value
+  }
+
+  // Add a new note to the top of the array
+  notes.value.unshift(note)
+
+  newNote.value = ''
+
+  newNoteRef.value.focus()
+}
 
 </script>
 
 
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea v-model="newNote" class="textarea" placeholder="Add a new note" ref="newNoteRef" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button @click="addNote" class="button is-link has-background-success" :disabled="!newNote">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- CARDS -->
+    <Note v-for="note in notes" :key="note.id" :note="note" />
+
   </div>
 </template>
 
