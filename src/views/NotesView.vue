3d07625e467597e6e09e52fb31ddedb088228cc4
Elvis <elviskb@gmail.com>
2022-11-23 14:54:08 +0300

    initial setup & bulma installation

---
+++ src/views/NotesView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="notes">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/StatsView.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <div class="stats">
+    <h1>Stats</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
